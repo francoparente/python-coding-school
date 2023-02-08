d933e7f2458b2c890fb5c8713b3fcea8fe2b2c35
Franco Daniel Parente <fparente14@gmail.com>
2023-02-08 11:36:55 -0300

    Create Test01.java

---
+++ java/java_example/src/test/java/Test01.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test01 {
+    @Test
+    void name() {
+        assertEquals(true,true);
+    }
+}
