7ebbbe35cee76b8ccd16ed405d5144d165096210
Franco Daniel Parente <fparente14@gmail.com>
2023-02-08 11:10:41 -0300

    Create ejemploColecciones.py

---
+++ python/EjemploColecciones/source/ejemploColecciones.py
@@ -0,0 +1,16 @@
+una_lista = ['a','b','a']
+
+una_lista.count('a')
+una_lista.index('b')
+len(una_lista)
+'a' in una_lista
+una_lista[0]
+una_lista.append('x')
+una_lista.insert(0,'z')
+
+
+un_set = {'a','b','a'}
+
+un_set.add('c')
+len(un_set)
+'a' in un_set
